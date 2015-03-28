fd291d928a61479a13f4b0ba3bebb136992445b5
KrishnaIndia <krishnaof1988@gmail.com>
2015-03-28 22:13:19 +0530

    including mod in main

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,7 @@
-extern crate rust_coverage_sample;
-
-use rust_coverage_sample::*;
+mod stud;
 
 fn main() {
   //println!("{:?}", add(1i32, 5i32));
-  let stud = rust_coverage_sample::stud::Student::new(10i32, 100u8);
+  let stud = stud::Student::new(10i32, 100u8);
   println!("Id : {:?}", stud.get_id());
 }
