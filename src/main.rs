0b092c78e3a27f613e82d247b8241597a2921be0
KrishnaIndia <krishnaof1988@gmail.com>
2015-03-28 22:08:02 +0530

    executable test

--- src/main.rs
+++ src/main.rs
@@ -4,5 +4,6 @@ use rust_coverage_sample::*;
 
 fn main() {
   //println!("{:?}", add(1i32, 5i32));
-  //rust_coverage_sample::lib_suite();
+  let stud = rust_coverage_sample::stud::Student::new(10i32, 100u8);
+  println!("Id : {:?}", stud.get_id());
 }
