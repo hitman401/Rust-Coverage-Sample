325bcc5c8676fb6ad21b7e8c4385e4a0426cb693
KrishnaIndia <krishnaof1988@gmail.com>
2015-03-28 20:26:06 +0530

    converted as lib

---
+++ src/lib.rs
@@ -0,0 +1,13 @@
+pub fn add(a: i32, b: i32) -> isize {
+	(a + b) as isize
+}
+
+pub fn sub(a: i32, b: i32) -> isize {
+	(a + b) as isize
+}
+
+
+#[test]
+fn add_test() {
+	assert_eq!(10isize, add(4, 6))
+}
