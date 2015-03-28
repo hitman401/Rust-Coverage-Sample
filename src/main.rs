32a46fee4fbe83dccfc5bdeb31fd80b859e664be
Krishna <krishnaof1988@gmail.com>
2015-03-28 23:24:24 +0530

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,6 @@ mod faculty;
 fn main() {
   //println!("{:?}", add(1i32, 5i32));
   let stud = stud::Student::new(10i32, 100u8);
-  //let fac = faculty::Faculty::new(100u8);
+  let fac = faculty::Faculty::new(100u8);
 //  println!("Id : {:?}", stud.get_id());
 }
